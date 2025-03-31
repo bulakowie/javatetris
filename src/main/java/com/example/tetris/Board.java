7aa22df6b16d2c6e75794360518124290388224f
bulakowie <tymoteuszdz@gmail.com>
2025-03-31 13:15:53 +0200

    Blocks move left and right

--- src/main/java/com/example/tetris/Board.java
+++ src/main/java/com/example/tetris/Board.java
@@ -9,12 +9,6 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.IOException;
@@ -26,11 +20,16 @@ public class Board extends Application {
     private static final int HEIGHT = 20; // Rozmiar tablicy (8x8)
     private static final int WIDTH = 10;
     private static final int SQUARE_SIZE = 30; // Rozmiar pojedynczego kwadratu
-    public int[][] TetrisBoard; ;
+    public int[][] TetrisBoard;
     public int[][] TetrisChange;
     public GridPane gridPane;
+
+
+
+
     @Override
     public void start(Stage primaryStage) {
+
         gridPane = new GridPane();
         TetrisBoard = new int[10][24];
 
@@ -46,14 +45,69 @@ public class Board extends Application {
         TetrisBoard[0][1]  = 2;
         TetrisBoard[0][2]  = 2;
         TetrisBoard[0][3]  = 2; */
-        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.05), e -> Update(primaryStage)));
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.04), e -> Update(primaryStage)));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.playFromStart();
 
         Scene scene = new Scene(gridPane, WIDTH* SQUARE_SIZE + 300, HEIGHT* SQUARE_SIZE);
-        primaryStage.setTitle("Chess Board");
+        primaryStage.setTitle("Tetris");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+
+    public void moveBlockLeft()
+    {
+        boolean canMove = true;
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT+3; j++) {
+                if (TetrisBoard[i][j] == 2) {
+                    if (i - 1 == -1) canMove = false;
+                    else if (TetrisBoard[i-1][j] == 1 ) canMove = false;
+                }
+            }
+        }
+        if (canMove)
+        {
+            TetrisChange = new int[10][23];
+
+            for (int i = 0; i < WIDTH; i++) {
+                for (int j = 0; j < HEIGHT+3; j++) {
+                    if (TetrisBoard[i][j] == 2) {
+                        TetrisChange[i-1][j] = 2;
+                    }
+                    if (TetrisBoard[i][j] == 1) TetrisChange[i][j] = 1;
+                }
+            }
+            TetrisBoard = TetrisChange;
+        }
+    }
+
+
+    public void moveBlockRight()
+    {
+        boolean canMove = true;
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT+3; j++) {
+                if (TetrisBoard[i][j] == 2) {
+                    if (i + 1 == 10) canMove = false;
+                    else if( TetrisBoard[i+1][j] == 1 ) canMove = false;
+                }
+            }
+        }
+        if (canMove)
+        {
+            TetrisChange = new int[10][23];
+            for (int i = 0; i < WIDTH; i++) {
+                for (int j = 0; j < HEIGHT+3; j++) {
+                    if (TetrisBoard[i][j] == 2) {
+                        TetrisChange[i+1][j] = 2;
+                    }
+                    if (TetrisBoard[i][j] == 1) TetrisChange[i][j] = 1;
+                }
+            }
+            TetrisBoard = TetrisChange;
+        }
     }
 
 
@@ -70,6 +124,7 @@ public class Board extends Application {
                 gridPane.add(square, col, row);
             }
         }
+
     }
 
 
@@ -77,6 +132,11 @@ public class Board extends Application {
 
     public void DropDown() {
         boolean BlockDropped = false;
+
+        Random random = new Random();
+        int n = random.nextInt(3);
+        if (n == 0) moveBlockLeft();
+        if (n == 1) moveBlockRight();
 
         TetrisChange = new int[10][23];
         for (int i = 0; i < WIDTH; i++) {
@@ -109,10 +169,15 @@ public class Board extends Application {
             isBlockDropping = false;
             BlockDropped = false;
         }
-
+    }
+    public void rotationRight()
+    {
 
     }
+    public void rotationLeft()
+    {
 
+    }
 
 
 
@@ -123,11 +188,19 @@ public class Board extends Application {
         Random random = new Random();
         int n = random.nextInt(7);
         Tetromino fallingBlock =  pool[n];
-        for (int i=0; i<3; i++)
-            for (int j = 0; j<3; j++)
+        for (int i=0; i<4; i++)
+            for (int j = 0; j<4; j++)
             {
-                    if (fallingBlock.tileRotationSchemes[0][i][j] == 1)
-                        TetrisBoard[i][j] = 2;
+                    if (i !=3 && j!=3) {
+                        if (fallingBlock.tileRotationSchemes[0][i][j] == 1)
+                            TetrisBoard[i][j] = 2;
+                    }
+                    else if (n== 4 || n== 6)
+                    {
+                        if (fallingBlock.tileRotationSchemes[0][i][j] == 1)
+                            TetrisBoard[i][j] = 2;
+
+                    }
             }
 
 
