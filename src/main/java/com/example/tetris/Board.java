ba3b6a439870d46c772a79fb6dadc00948c8248a
bulakowie <tymoteuszdz@gmail.com>
2025-04-01 18:40:45 +0200

    Blocks Rotate and its working! (mostly)

--- src/main/java/com/example/tetris/Board.java
+++ src/main/java/com/example/tetris/Board.java
@@ -23,13 +23,13 @@ public class Board extends Application {
     public int[][] TetrisBoard;
     public int[][] TetrisChange;
     public GridPane gridPane;
-
-
-
-
+    Tetromino activeBlock;
+    public int rotationValue;
+    public  boolean penis;
+    public boolean isBlockBig;
     @Override
     public void start(Stage primaryStage) {
-
+penis = true;
         gridPane = new GridPane();
         TetrisBoard = new int[10][24];
 
@@ -45,7 +45,7 @@ public class Board extends Application {
         TetrisBoard[0][1]  = 2;
         TetrisBoard[0][2]  = 2;
         TetrisBoard[0][3]  = 2; */
-        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.04), e -> Update(primaryStage)));
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.2), e -> Update(primaryStage)));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.playFromStart();
 
@@ -114,6 +114,7 @@ public class Board extends Application {
 
     public void Update(Stage primaryStage)
     {
+
         if(!isBlockDropping) newBlock();
         DropDown();
         for (int row = 0; row < HEIGHT; row++) {
@@ -135,48 +136,104 @@ public class Board extends Application {
 
         Random random = new Random();
         int n = random.nextInt(3);
-        if (n == 0) moveBlockLeft();
-        if (n == 1) moveBlockRight();
-
-        TetrisChange = new int[10][23];
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT+3; j++) {
-                if (TetrisBoard[i][j] == 2) {
-                    TetrisChange[i][j + 1] = 2;
-                    if (j + 1 > HEIGHT || TetrisBoard[i][j + 2] == 1) BlockDropped = true;
-                }
-                if (TetrisBoard[i][j] == 1) TetrisChange[i][j] = 1;
-            }
-        }
-        if (BlockDropped) {
-          //  System.out.println(123.456);
+        if (n == 0) rotation(1);
+        if (n == 1)rotation(2);
+        else {
+            TetrisChange = new int[10][23];
             for (int i = 0; i < WIDTH; i++) {
-                for (int j = 0; j < HEIGHT+3; j++) {
-                    if(TetrisChange[i][j] == 2) {
-                       // System.out.println(123.456);
-                        TetrisChange[i][j] = 1;
+                for (int j = 0; j < HEIGHT + 3; j++) {
+                    if (TetrisBoard[i][j] == 2) {
+                        TetrisChange[i][j + 1] = 2;
+                        if (j + 1 > HEIGHT || TetrisBoard[i][j + 2] == 1) BlockDropped = true;
+                    }
+                    if (TetrisBoard[i][j] == 1) TetrisChange[i][j] = 1;
+                }
+            }
+            if (BlockDropped) {
+                //  System.out.println(123.456);
+                penis = true;
+                for (int i = 0; i < WIDTH; i++) {
+                    for (int j = 0; j < HEIGHT + 3; j++) {
+                        if (TetrisChange[i][j] == 2) {
+                            // System.out.println(123.456);
+                            TetrisChange[i][j] = 1;
+                        }
                     }
                 }
             }
-        }
-        TetrisBoard = TetrisChange;
-        if (BlockDropped)
-        {
+            TetrisBoard = TetrisChange;
+            if (BlockDropped) {
            /* TetrisBoard[3][0]  = 2;
             TetrisBoard[3][1]  = 2;
             TetrisBoard[3][2]  = 2;
             TetrisBoard[4][2]  = 2; */
-            isBlockDropping = false;
-            BlockDropped = false;
+                isBlockDropping = false;
+                BlockDropped = false;
+            }
         }
     }
-    public void rotationRight()
+    public void rotation(int n)
     {
+      //  if (n == 1) System.out.print("lewo");
+       // else System.out.print("prawo");
 
-    }
-    public void rotationLeft()
-    {
 
+        boolean ROTATE = true;
+        //n - 1 : lewo
+        int placeholderRotationValue;
+        //n - 2 : prawo
+        int topUp= 9999; int topLeft = 9999;
+        int [][]TetrisChange2 =new int[10][23];
+
+        if (n==1) placeholderRotationValue = rotationValue +1;
+        else placeholderRotationValue = rotationValue -1;
+        if (placeholderRotationValue == -1) placeholderRotationValue = 3;
+        if (placeholderRotationValue == 4) placeholderRotationValue = 0;
+        //System.out.println(placeholderRotationValue);
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT+3; j++) {
+                if(TetrisBoard[i][j] == 2) {
+                    if (j<topUp) topUp =j;
+                    if (i<topLeft) topLeft = i;
+                }
+                if (TetrisBoard[i][j]==1) TetrisChange2[i][j]=1;
+            }
+        }
+        System.out.println(topUp + " " + topLeft);
+        for (int i=0; i<4; i++)
+            for (int j = 0; j<4; j++)
+            {
+                if (i !=3 && j!=3) {
+                    if (activeBlock.tileRotationSchemes[placeholderRotationValue][i][j] == 1)
+                    {
+                        if (i+topLeft>9 || j+topUp >22) ROTATE = false;
+                        else if (TetrisBoard[i+topLeft][j+topUp]==1) ROTATE = false;
+                        else TetrisChange2[i+topLeft][j+topUp] = 2;
+                    }
+                }
+                else if (isBlockBig)
+                {
+                    if (activeBlock.tileRotationSchemes[placeholderRotationValue][i][j] == 1)
+                    {
+                        if (i+topLeft>9 || j+topUp >21) ROTATE = false;
+                        else if (TetrisBoard[i+topLeft][j+topUp]==1) ROTATE = false;
+                        else TetrisChange2[i+topLeft][j+topUp] = 2;
+                    }
+                }
+                if (penis)
+                {
+                    if (!ROTATE) System.out.println(i+topLeft);
+                    if (!ROTATE) System.out.println(j+topUp);
+                    if (!ROTATE) penis = false;
+                }
+
+
+            }
+    //    System.out.println("penisisko");
+    if (ROTATE)
+    {TetrisBoard = TetrisChange2;
+        rotationValue = placeholderRotationValue;}
+    else System.out.println("penisisko");
     }
 
 
@@ -188,20 +245,25 @@ public class Board extends Application {
         Random random = new Random();
         int n = random.nextInt(7);
         Tetromino fallingBlock =  pool[n];
+        System.out.println(fallingBlock.toString());
         for (int i=0; i<4; i++)
             for (int j = 0; j<4; j++)
             {
                     if (i !=3 && j!=3) {
                         if (fallingBlock.tileRotationSchemes[0][i][j] == 1)
-                            TetrisBoard[i][j] = 2;
+                            TetrisBoard[i][j+1] = 2;
+                        isBlockBig = false;
                     }
                     else if (n== 4 || n== 6)
                     {
+                        isBlockBig = true;
                         if (fallingBlock.tileRotationSchemes[0][i][j] == 1)
-                            TetrisBoard[i][j] = 2;
+                            TetrisBoard[i][j+1] = 2;
 
                     }
             }
+        activeBlock = fallingBlock;
+        rotationValue = 0;
 
 
 
